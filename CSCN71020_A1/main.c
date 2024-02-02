96f2c5cb244fb1ae970050f3c2deb54eb7894615
Mohammed Sujahat Ali <mali1237@conestogac.on.ca>
2024-02-02 11:03:56 -0500

    added void subtract()

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,45 +3,58 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
-
+void subtract(); // Declare the subtract function
 
 void main() {
+    printWelcomeMenu();
+    printOptions();
 
-	printWelcomeMenu();
+    int inputNum;
 
-	printOptions();
+    printf("Enter operation number: ");
+    scanf_s("%d", &inputNum); // Change %1o to %d for reading integers
 
-	int inputNum;
-
-	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
-
-	switch (inputNum)
-	{
-	case 1:
-		add();
-	}
-
+    switch (inputNum) {
+    case 1:
+        add();
+        break;
+    case 2:
+        subtract(); // Call the subtract function for case 2
+        break;
+    default:
+        printf("Invalid operation number\n");
+        break;
+    }
 }
 
 void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   BCS Calculator   **\n");
-	printf(" **********************\n");
+    printf(" **********************\n");
+    printf("**   Welcome to the   **\n");
+    printf("**   BCS Calculator   **\n");
+    printf(" **********************\n");
 }
 
 void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
+    printf("1. Add\n");
+    printf("2. Subtract\n");
 }
 
 void add() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 + num2;
+    printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 - num2;
+    printf("%lf - %lf = %lf\n", num1, num2, result);
 }
